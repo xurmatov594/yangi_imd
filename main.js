097d54b0dc4fa274a29528589663d788f3d2ad94
xurmatov594 <javohirxurmatov@gmail.com>
2021-12-12 16:59:41 +0300

    initil commit

---
+++ main.js
@@ -0,0 +1,393 @@
+var kino = [{
+        "id": 1,
+        "name": "Qodirxon-1",
+        "img": "https://i.ytimg.com/vi/WjY-Wv9ppFg/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLD5s_JODMmXtmLROqA4x138i8OpBQ",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=WjY-Wv9ppFg",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-2",
+        "img": "https://i.ytimg.com/vi/gRaxf2lIN78/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAEqiJj9Pc9wVnvbuGV98hwsS3qsA",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=gRaxf2lIN78",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-3",
+        "img": "https://i.ytimg.com/vi/7nYpP9q3-Og/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBFK-X43abXbk1xe9S-JyANJ6FvkQ",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=7nYpP9q3-Og",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-4",
+        "img": "https://i.ytimg.com/vi/8tvuFEy8Pj4/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAojgpxv1j__cjBNHL_X43Iy89C4Q",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=8tvuFEy8Pj4",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-5",
+        "img": "https://i.ytimg.com/vi/tZGdBBj_odo/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDPDJmA_U7S7XDP6IQkj-NnufI93g",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=tZGdBBj_odo",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-6",
+        "img": "https://i.ytimg.com/vi/Oue00I-UftI/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBQ2W3yRPD48Wz3vDt09n3ZMN-KkA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=Oue00I-UftI",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-7",
+        "img": "https://i.ytimg.com/vi/ahH5vwolyNA/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBgnPDDebI41DUwyu4mq7H5Id0UBQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=ahH5vwolyNA",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-8",
+        "img": "https://i.ytimg.com/vi/luKlBDF3GYU/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAFvO9LfgDX11LzYWbnFvNVYulJWA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=luKlBDF3GYU",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-9",
+        "img": "https://i.ytimg.com/vi/AjyAdk7h2xs/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAc0hqi_LQf0KYxL_EpNsIgcNC2PA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=AjyAdk7h2xs",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-10",
+        "img": "https://i.ytimg.com/vi/DCi3SqtZk4k/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLC3zVQCZFKXx_1NnDSkyJct8isLnQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=DCi3SqtZk4k",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-11",
+        "img": "https://i.ytimg.com/vi/DCi3SqtZk4k/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLC3zVQCZFKXx_1NnDSkyJct8isLnQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=SyBTfq2O5rg",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-12",
+        "img": "	https://i.ytimg.com/vi/RTibyWL804Y/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCNsuBbKjnVJ_IGZ_vMAZDB3ObTcQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=RTibyWL804Y",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-13",
+        "img": "https://i.ytimg.com/vi/RTibyWL804Y/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCNsuBbKjnVJ_IGZ_vMAZDB3ObTcQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=F0v2vqrw8BY",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-14",
+        "img": "https://i.ytimg.com/vi/XEe-_8IRfIk/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLB8wIacPwpF23Grz7uqBHpolMEOig",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=XEe-_8IRfIk",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-15",
+        "img": "https://i.ytimg.com/vi/JX8o4Fc1xKg/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDu-C-mLg5ywSMV52B6eaIDkBhyAA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=JX8o4Fc1xKg",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-16",
+        "img": "https://i.ytimg.com/vi/qR9UGlAUKVQ/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCh3LyWPelLoW8Gg2V9yfCKgJXIwg",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=qR9UGlAUKVQ",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-17",
+        "img": "	https://i.ytimg.com/vi/NEOgDry-e6Q/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBjl5izrByN7Bier2eVC9y_oQCu-A",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=NEOgDry-e6Q",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-18",
+        "img": "https://i.ytimg.com/vi/Xzn3L02zgZ4/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLC-clFBBSC8AxqOgubiNBB2sBDIPA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=Xzn3L02zgZ4",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon19",
+        "img": "https://i.ytimg.com/vi/uN5PS40t5yA/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLAPeFPCVZODgA8vMPzvoRb7ccJAaQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=uN5PS40t5yA",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-20",
+        "img": "https://i.ytimg.com/vi/m4xJJVnoiCI/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLBYTf0PZAzrj226Pf7CwlDX81DOaQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=m4xJJVnoiCI",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-21",
+        "img": "https://i.ytimg.com/vi/j27Pu2oJV-M/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCpN4ogW1X0MR2MfKSMWT9PY2cfdw",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=j27Pu2oJV-M",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-22",
+        "img": "https://i.ytimg.com/vi/oQ1b-plVRbs/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCo4XaxBOf4Z6Ts_8XkgXrWoFmjig",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=oQ1b-plVRbs",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-23",
+        "img": "	https://i.ytimg.com/vi/SJb98LMXsPk/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDPv56aisXSlmRGzUrWvVkfytkUUA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=SJb98LMXsPk",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-24",
+        "img": "https://i.ytimg.com/vi/wX62rVKNEzQ/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCo4dWQ4z1m43BTjtVasfZpz37r6w",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=wX62rVKNEzQ",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-25",
+        "img": "https://i.ytimg.com/vi/wp1BDsmlkeA/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDlIJ-7tHsjsKPQs5E8FM3Je1ajbg",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=wp1BDsmlkeA",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-26",
+        "img": "https://i.ytimg.com/vi/piowuxiQbFU/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCJOHVeJNybFExFuRGmw4H8NzmcpA",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=piowuxiQbFU",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-27",
+        "img": "https://i.ytimg.com/vi/-OSmNg3ipr8/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLDMWdjFqPKnLFrdAS1jcxBL-mKELQ",
+        "title": "Serial",
+         "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+         "link":"https://www.youtube.com/watch?v=-OSmNg3ipr8",
+         "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-28",
+        "img": "https://i.ytimg.com/vi/Kt0jpbCEO_I/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCf9T51ShlRwI7f2IM-jWha16CGOQ",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=Kt0jpbCEO_I",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-29",
+        "img": "https://i.ytimg.com/vi/yvayX8F8hUQ/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLC4KeucJr3UGDOmWY_Tp__Scz1poA",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=yvayX8F8hUQ",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+    {
+        "id": 1,
+        "name": "Qodirxon-30",
+        "img": "https://i.ytimg.com/vi/5EaJy7C6xeU/hq720.jpg?sqp=-…AFwAcABBg==&rs=AOn4CLCW_raOFje0ucHOaMZfFLAEjLI21Q",
+        "title": "Serial",
+        "play":"https://png.pngtree.com/png-vector/20190223/ourlarge/pngtree-vector-play-icon-png-image_695746.jpg",
+        "link":"https://www.youtube.com/watch?v=5EaJy7C6xeU",
+        "ganre": [
+            "comedy", "dramma"
+        ]
+    },
+
+];
+for (var i = 0; i < kino.length; i++) {
+    var elul = document.querySelector('.ul');
+    var elli = document.createElement('li');
+    var elimg = document.createElement('img');
+    var elspan = document.createElement('span');
+    var ela = document.createElement('a');
+    var elimgg = document.createElement('img');
+  
+    var elp = document.createElement('p');
+    var elpa = document.createElement('p');
+    var elpar = document.createElement('p');
+    
+
+    elimg.src = kino[i].img;
+    elp.textContent = kino[i].name;
+    elpa.textContent = kino[i].ganre;
+    elpar.textContent = kino[i].title;
+    elimgg.src = kino[i].play;
+    ela.href=kino[i].link;
+
+
+
+    elul.appendChild(elli);
+    elli.appendChild(elimg);
+    elli.appendChild(elspan)
+    elspan.appendChild(ela);
+    ela.appendChild(elimgg);
+    elli.appendChild(elp);
+    elli.appendChild(elpa);
+    elli.appendChild(elpar);
+   
+}
